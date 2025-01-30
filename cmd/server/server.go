7e7d79343f1b1dada151b873d63dea6faa2391a2
Aix <Aix60689100@gmail.com>
2025-01-30 15:59:53 +0200

    Create Server

---
+++ cmd/server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+type SrvCfg struct {
+	Port                string
+	ReaderHeaderTimeOut time.Duration
+	WriterHeaderTimeOut time.Duration
+	IdleTimeout         time.Duration
+}
+
+func (s *Server) RunServer(cfg SrvCfg, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:         cfg.Port,
+		Handler:      handler,
+		ReadTimeout:  cfg.ReaderHeaderTimeOut,
+		WriteTimeout: cfg.WriterHeaderTimeOut,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
