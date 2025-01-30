709dd1513a727c097ade5fea57b22d24ab2f11c7
Aix <Aix60689100@gmail.com>
2025-01-30 22:34:15 +0200

    Add utility functions for request validation, JSON parsing, and seller ID extraction

--- onlineStore/handler/utils.go
+++ onlineStore/handler/utils.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func CheckContentType(w http.ResponseWriter, r *http.Request) bool {
@@ -21,4 +24,29 @@ func ParseJSONBody(w http.ResponseWriter, r *http.Request, dst interface{}) bool
 	}
 
 	return true
+}
+
+func ValidateSeller(name, phone string) error {
+	if name == "" {
+		return fmt.Errorf("name is required")
+	}
+	if phone == "" {
+		return fmt.Errorf("phone is required")
+	}
+
+	return nil
+}
+
+func getSellerID(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+	if idStr == "" {
+		return 0, fmt.Errorf("seller ID is required")
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, fmt.Errorf("invalid seller ID")
+	}
+
+	return id, nil
 }
